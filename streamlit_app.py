70cefe22ba3b2f427d3c5064ca51e81a22583b0c
NAZISH-26 <131849154+NAZISH-26@users.noreply.github.com>
2023-06-15 17:25:50 +0530

    Update streamlit_app.py
    breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title ('My parents new healthy diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Bluberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
