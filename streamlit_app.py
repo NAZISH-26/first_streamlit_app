2a1326bb824a0ff3a3503eb050a39820bab1b8d5
NAZISH-26 <131849154+NAZISH-26@users.noreply.github.com>
2023-06-19 10:39:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,9 @@ streamlit.text('🥑🍞 Avacado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+# The picker works, but the numbers don't make any sense! We want the customer to be able to choose the fruits by name!!
+# so set index to with fruit column
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
