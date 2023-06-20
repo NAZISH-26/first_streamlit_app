d5f16a59ee2879a91057d235e5a82fc176b92fc3
NAZISH-26 <131849154+NAZISH-26@users.noreply.github.com>
2023-06-20 16:12:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,8 +33,11 @@ streamlit.dataframe(fruits_to_show)
 # Display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
 # using API and variabled
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 # Display raw json output 
 # streamlit.text(fruityvice_response.json())
 
